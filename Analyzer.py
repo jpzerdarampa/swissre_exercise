2d02f157150e5d82ed84df625deb54a50dea2be3
Juan Pablo Zerda Rampa <juanpablo.zerdarampa.ext@telefonica.com>
2022-09-22 07:07:08 +0200

    Creation of output and operation classes

--- Analyzer.py
+++ Analyzer.py
@@ -1,14 +1,38 @@
 from argparse import ArgumentParser
-import re
+from abc import ABC, abstractmethod
+from collections import Counter
+import re, json
+import pdb
+
+
+class OutputLoader(object):
+    def __init__(self, output_path):
+        self.output = str(output_path)
+
+    def get_output(self):
+        return self.output
+
+
+#The input parameter 'operation' of the class Json_output_Loader, is an object of an operation class which contains
+#the returned data, and name of the class
+class Json_output_Loader(OutputLoader):
+    def __init__(self, output_path, operation):
+        OutputLoader.__init__(self, output_path)
+
+        data_output_dict: dict = dict()
+        data_output_dict[operation.name] = operation.data_output
+        with open(self.get_output(), 'w') as json_file:
+            json.dump(data_output_dict, json_file)
 
 
 # Base class. It will store common input for any existing loader (other formats)
 class LogsLoader(object):
     def __init__(self, data_input):
-        self.input = str(data_input)
+        #This attribute store the path of the input file. It will be a common attribute for all the inherited classes
+        self._input = str(data_input)
 
     def get_input(self):
-        return self.input
+        return self._input
 
 
 # Class in charge of loading an input file, and run the appropiate method according to the log formats
@@ -26,6 +50,7 @@ class CSV_Loader(LogsLoader):
         self.field_8: list = list()
         self.field_9: list = list()
         self.field_10: list = list()
+        self.csv_decode()
 
     def csv_decode(self):
         # with open(super()._input, 'r') as file:
@@ -38,8 +63,10 @@ class CSV_Loader(LogsLoader):
                     line_text = file.readline()
                     dissection = re.split('\s\s+', line_text)
                     if len(dissection) < 2:     #There are specific cases in which there is only one space between the first and second field
-                        fields_list = line_text.split(' ')
-                        self.field_1.append(fields_list[0])
+                        aux_list = line_text.split(' ')
+                        self.field_1.append(aux_list[0])
+                        del aux_list[0]
+                        fields_list = aux_list
                     else:
                         fields_list = dissection[1].split(' ')
                         self.field_1.append(dissection[0])
@@ -58,7 +85,93 @@ class CSV_Loader(LogsLoader):
         except Exception as e:
             print("Error: %s", str(e))
 
-        print("El numero de registros es: ", line_number)
+        print("Number of logs: ", line_number)
+
+
+#  The Operation interface declares a method action() for building the inherited classes.
+# Future operations (inherited classes) must contain an action() method to implement the new funcionality and return a string
+class Operation(ABC):
+    @abstractmethod
+    def action(self, request) -> str:
+        pass
+
+
+class MostFrequentIP(Operation):
+    def __init__(self, LogsLoader):
+        self.LogsLoader = LogsLoader
+        self.data_output = self.action()        #Will be used as the value of the returned json
+        self.name = self.__class__.__name__     #Will be used as the key of the returned json
+
+    def action(self):
+        counter = Counter(self.LogsLoader.field_3)
+        ordered_elements = counter.most_common()
+        first = ordered_elements[0]
+
+        return str(first[0])
+
+
+class LeastFrequentIP(Operation):
+    def __init__(self, LogsLoader):
+        self.LogsLoader = LogsLoader
+        self.data_output = self.action()  # Will be used as the value of the returned json
+        self.name = self.__class__.__name__  # Will be used as the key of the returned json
+
+    def action(self):
+        counter = Counter(self.LogsLoader.field_3)
+        ordered_elements = counter.most_common()
+        last = ordered_elements[-1]
+
+        #There are more ip with 1 occurence. In this version we will take only one of them
+        return str(last[0])
+
+
+#This will return the average number of frames/sec
+class EventsperSecond(Operation):
+    def __init__(self, LogsLoader):
+        self.LogsLoader = LogsLoader
+        self.data_output = self.action()  # Will be used as the value of the returned json
+        self.name = self.__class__.__name__  # Will be used as the key of the returned json
+
+    def action(self):
+        timestamp_sec = self.remove_decimal_part()
+        counter = Counter(timestamp_sec)
+        ordered_elements = counter.most_common()
+
+        number_occurences = 0
+        number_elements = 0
+        for element in ordered_elements:
+            number_occurences = number_occurences + element[1]
+            number_elements = number_elements + 1
+
+        events_per_sec = number_occurences/number_elements
+
+        return events_per_sec
+
+    def remove_decimal_part(self):
+        timestamp_sec: list = list()
+        for timestamp in self.LogsLoader.field_1:
+            timestamp_sec.append(timestamp.split('.')[0])
+
+        return timestamp_sec
+
+
+# I will assume that total amount of bytes of each log, is the sum of the header size(field 2) and the body size (field 5)
+class TotalAmountofBytes(object):
+    def __init__(self, LogsLoader):
+        self.LogsLoader = LogsLoader
+        self.data_output = self.action()  # Will be used as the value of the returned json
+        self.name = self.__class__.__name__  # Will be used as the key of the returned json
+
+    def action(self):
+        header_bytes_accumulated = 0
+        for header_bytes in self.LogsLoader.field_2:
+            header_bytes_accumulated = header_bytes_accumulated + int(header_bytes)
+
+        body_bytes_accumulated = 0
+        for body_bytes in self.LogsLoader.field_5:
+            body_bytes_accumulated = body_bytes_accumulated + int(body_bytes)
+
+        return header_bytes_accumulated + body_bytes_accumulated
 
 
 def parse_arguments():
@@ -67,8 +180,8 @@ def parse_arguments():
 
     parser = ArgumentParser(description='Script to analyze logs for SwissRe')
 
-    parser.add_argument('-i', '--input', help='Path to a plain text which contain logs')
-    parser.add_argument('-o', '--output', help='Path to a file to save output in plain text JSON format')
+    parser.add_argument('-i', '--input', default=None, help='Path to a plain text which contain logs')
+    parser.add_argument('-o', '--output', default=None, help='Path to a file to save output in plain text JSON format')
 
     group = parser.add_mutually_exclusive_group()
     group.add_argument('-m', '--most_frequent', action='store_true', help='Most frequent IP')
@@ -86,25 +199,30 @@ def main():
     params = parse_arguments()
 
     if (params.input and params.output) is not None:
-        input = params.input
-        output = params.output
+        input_path = params.input
+        output_path = params.output
     else:
         print('Input and Output arguments are mandatories. Please re-introduce them')
         return False
 
-    CSV_Loader(input).csv_decode()
+    # The class CSV_Loader loads files with CSV format. If we want to use other input files with diferent format,
+    # we should create a new class inherited from the base class LogsLoader
+    input_load = CSV_Loader(input_path)
 
+    # Operations. Operation and input classes are link throughout aggregation relationship
     if params.most_frequent is not False:
-        pass
+        operation = MostFrequentIP(input_load)
 
     elif params.least_frequent is not False:
-        pass
+        operation = LeastFrequentIP(input_load)
 
     elif params.events_second is not False:
-        pass
+        operation = EventsperSecond(input_load)
 
     elif params.total_bytes is not False:
-        pass
+        operation = TotalAmountofBytes(input_load)
+
+    Json_output_Loader(output_path, operation)
 
 
 if __name__ == "__main__":
